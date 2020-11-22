5ea6db15360378039aef8f89a5a5d2ea722c4e4a
Vlad <vlad.panchenko@gmail.com>
2020-11-22 22:43:34 +0300

    15-Fixed git ignore issue with ignoring SQL folder

---
+++ CoreMicroservice/Microservice.Core/Infrastructure/UnitOfWork/SQL/ISQLUnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microservice.Core.Infrastructure.UnitofWork.SQL
+{
+    public interface ISQLUnitOfWork
+    {
+        public void CreateTransaction();
+
+        public Task CreateTransactionAsync();
+
+        public void Rollback();
+
+        public Task RollbackAsync();
+
+        public void Save();
+
+        public Task SaveAsync();
+
+        public TRepository GetRepository<TRepository>() where TRepository : class;
+    }
+}
